ef2da2b1015fad209a2b5e1ffb9d527dcc56e826
Alchemist75 <407982040@qq.com>
2021-03-05 19:02:54 -0500

    added azureocr

---
+++ scripts/const.py
@@ -0,0 +1,5 @@
+IMG_PATH = "/mnt/d/Downloads/train_images"
+AZURE_OCR_PATH = "/mnt/d/Downloads/train_images_AZOCR/"
+VAL_JSON = "/mnt/d/Downloads/train_images/TextVQA_0.5.1_val.json"
+OCR_JSON = "/mnt/d/Downloads/train_images/TextVQA_Rosetta_OCR_v0.2_val.json"
+PRED_JSON = '../snap/textvqa_m4c_28662719/reports/textvqa_run_val_2021-02-28T16:21:14.json'
