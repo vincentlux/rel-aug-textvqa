1c0b2c0b4b6a61899d22f5ea5bbb6689d66c99d0
vincentlux <vincentlu073@gmail.com>
2021-05-14 21:31:50 -0400

    change obj token default to true

--- scripts/convert_obj_npz_to_lmdb.py
+++ scripts/convert_obj_npz_to_lmdb.py
@@ -42,24 +42,34 @@ def load_vocab_file(filename):
 
 if __name__ == '__main__':
     # load and see what is the format of open_images/detectron.lmdb
-    old_data_name = 'data/data/datasets/stvqa/defaults/features/detectron.lmdb'
-    loader = LMDBLoader(old_data_name)
-    for imgid in loader.get_image_ids():
-        pdb.set_trace()
-        with loader.env.begin(write=False, buffers=True) as txn:
-            image_info = pickle.loads(txn.get(imgid))
-        print(image_info)
+    # old_data_name = 'data/data/datasets/stvqa/defaults/features/detectron.lmdb'
+    # loader = LMDBLoader(old_data_name)
+    # for imgid in loader.get_image_ids():
+    #     pdb.set_trace()
+    #     with loader.env.begin(write=False, buffers=True) as txn:
+    #         image_info = pickle.loads(txn.get(imgid))
+    #     print(image_info)
 
     # ['feature_path', 'features', 'image_height', 'image_width', 'num_boxes', 'objects', 'cls_prob', 'bbox']
-    mode = 'test' # 'train'
-    npz_folder = '/home/vincent/proj/hw/11797/data/textvqa/test_images_frcnn'
-    tgt_name = 'data/data/datasets/textvqa/defaults/features/open_images/detectron_attrs_max50_v0_test.lmdb'
+    dataset = 'stvqa' # 'textvqa'
+    if dataset == 'stvqa':
+        mode = None # stvqa does not need append mode in the begining
+        npz_folder = '/home/vincent/proj/hw/11797/data/stvqa/train_images_frcnn/'
+        tgt_name = 'data/data/datasets/stvqa/defaults/features/detectron_attrs_max50_v0.lmdb'
+        npz_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(npz_folder) for f in fn]
+        # silly way of removing root path
+        npz_files = [i.replace(npz_folder, '') for i in npz_files]
+    elif dataset == 'textvqa':
+        mode = 'test' # 'train'
+        npz_folder = '/home/vincent/proj/hw/11797/data/textvqa/test_images_frcnn'
+        tgt_name = 'data/data/datasets/textvqa/defaults/features/open_images/detectron_attrs_max50_v0_test.lmdb'
+        npz_files = os.listdir(npz_folder)
 
     vg_objs = load_vocab_file('scripts/objects_vocab.txt')
     vg_attr = load_vocab_file('scripts/attributes_vocab.txt')
 
     key_info_map = {}
-    for npz in tqdm(os.listdir(npz_folder)):
+    for npz in tqdm(npz_files):
         if npz.endswith('npz'):
             filename = os.path.join(npz_folder, npz)
             data = np.load(filename, allow_pickle=True)
@@ -67,8 +77,13 @@ if __name__ == '__main__':
 
             features = data['x']
             bbox = data['bbox']
-            key = '{}/{}'.format(mode, meta['image_id'])
-            feature_path = '{}/{}'.format(mode, meta['image_id'])
+            if dataset == 'textvqa':
+                key = '{}/{}'.format(mode, meta['image_id'])
+                feature_path = '{}/{}'.format(mode, meta['image_id'])
+            elif dataset == 'stvqa':
+                key = meta['image_id']
+                feature_path = meta['image_id']
+
             image_height = meta['image_h']
             image_width = meta['image_w']
             num_boxes = meta['num_boxes']
